8e20b35e6a6561d35f7f767ddb438273d9f07b27
njar <njar@gmail.com>
2023-03-13 12:22:16 +0100

    modif

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          lorem sdfsd dqsdfds dffds
         </p>
         <a
           className="App-link"
