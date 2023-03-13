0d19564ef58ba2bced404d00574225d1d85a326e
njar <njar@gmail.com>
2023-03-13 12:28:02 +0100

    4eme

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and deploy
+          Learn React and deploy.
         </a>
       </header>
     </div>
